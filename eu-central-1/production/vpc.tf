93b23e14ab199e03443877b6b481a599aabae10f
mayowa akinyele <mayowaakinyele@ip-192-168-2-196.eu-central-1.compute.internal>
2026-01-23 11:44:33 +0100

    fix ci

--- eu-central-1/production/vpc.tf
+++ eu-central-1/production/vpc.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "secure-production-sg" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.secure-production-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
