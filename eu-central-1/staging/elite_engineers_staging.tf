03c409a572ff7ed95aa6a20cdcf91f4a51d1f6c1
chizobaeze <chizobaeze15@gmail.com>
2026-02-11 00:56:22 +0000

    create staging area

---
+++ eu-central-1/staging/elite_engineers_staging.tf
@@ -0,0 +1,9 @@
+
+module "elite_engineers_staging_zone" {
+  source          = "../modules/s3-bucket"
+  team            = "elite"
+  service         = "airflow"
+  bucket_use_case  = "staging-zone" 
+  versioning      = "Enabled"
+  environment     = var.environment
+}
