52912916fe921a097355b7d75b7b25cac93c11fc
chizobaeze <chizobaeze15@gmail.com>
2026-02-11 20:21:52 +0000

    create staging zone

--- eu-central-1/staging/elite_staging_data_lake.tf
+++ eu-central-1/staging/elite_staging_data_lake.tf
@@ -3,6 +3,6 @@ module "elite_engineers_staging_zone" {
   team            = "elite"
   service         = "airflow"
   bucket-use-case  = "data-lake" 
-  versioning      = false
+  versioning      = "Disabled"
   environment     = var.environment
 }
