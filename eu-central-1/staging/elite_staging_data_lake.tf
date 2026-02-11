a4bd885a131472111870f74b0eac6d5270b4befa
chizobaeze <chizobaeze15@gmail.com>
2026-02-11 18:54:43 +0000

    create staging zone

--- eu-central-1/staging/elite_staging_data_lake.tf
+++ eu-central-1/staging/elite_staging_data_lake.tf
@@ -2,7 +2,7 @@ module "elite_engineers_staging_zone" {
   source          = "../modules/s3-bucket"
   team            = "elite"
   service         = "airflow"
-  bucket_use_case  = "staging-zone" 
+  bucket-use-case  = "data-lake" 
   versioning      = "Enabled"
   environment     = var.environment
 }
