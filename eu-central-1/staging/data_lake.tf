0e44a9039cda91e305f979f9345ec51b695d9f63
mayowa akinyele <mayowaakinyele@ip-192-168-2-196.eu-central-1.compute.internal>
2026-02-13 16:34:51 +0100

    fix approval

--- eu-central-1/staging/data_lake.tf
+++ eu-central-1/staging/data_lake.tf
@@ -3,3 +3,9 @@ resource "aws_s3_bucket" "example" {
 
   tags = local.common_tags
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "staging-federated-engineers-adhoc-1"
+
+  tags = local.common_tags
+}
--- eu-central-1/staging/elite_staging_data_lake.tf
+++ eu-central-1/staging/elite_staging_data_lake.tf
@@ -2,7 +2,7 @@ module "elite_engineers_staging_zone" {
   source          = "../modules/s3-bucket"
   team            = "elite"
   service         = "airflow"
-  bucket-use-case  = "data-lake" 
+  bucket-use-case = "data-lake"
   versioning      = "Suspended"
   environment     = var.environment
 }
