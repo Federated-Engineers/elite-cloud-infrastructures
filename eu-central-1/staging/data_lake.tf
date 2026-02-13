03a3d5d5d7eef145540f436d7689154a2cf60fdf
mayowa akinyele <mayowaakinyele@ip-192-168-2-196.eu-central-1.compute.internal>
2026-02-13 17:27:31 +0100

    adding repos.yaml

---
+++ eu-central-1/staging/data_lake.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "staging-federated-engineers-adhoc"
+
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket" "example-1" {
+  bucket = "staging-federated-engineers-adhoc-1"
+
+  tags = local.common_tags
+}
