9edb4e67b6fb7db635173bc88cf5960c588e1afb
Musili-Adebayo <adebayomusili@gmail.com>
2026-02-19 21:13:19 +0100

    renamed vpc endpoint resource gateway

--- eu-central-1/production/elite_vpc_endpoint.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc_endpoint" "s3_gateway" {
-  vpc_id            = aws_vpc.elite-vpc.id
-  service_name      = "com.amazonaws.${var.region}.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.public-subnet-rtb.id]
-
-  tags = merge(local.common_tags, { Name : "elite-vpc-public-subnet-rtb" })
-}
