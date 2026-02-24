854f181da7bb35d9493a2e072004a851dce1b1df
Jhoie <victoriousjvictor@gmail.com>
2026-02-24 16:59:12 +0100

    Create elite-airflow-ecr

---
+++ eu-central-1/production/elite_ecr.tf
@@ -0,0 +1,14 @@
+resource "aws_ecr_repository" "elite_airflow" {
+  name                 = "elite-airflow"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = merge(local.common_tags, { Name = "elite-airflow-ecr" })
+}
