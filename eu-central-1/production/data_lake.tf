817e9585ae7d97449ee5e8c9492e2a2018cd55ca
Musili-Adebayo <adebayomusili@gmail.com>
2026-03-07 16:05:57 +0100

    created balearic blue superyacht services s3 bucket

--- eu-central-1/production/data_lake.tf
+++ eu-central-1/production/data_lake.tf
@@ -16,5 +16,13 @@ module "nordic_peak_bucket" {
   environment     = var.environment
 }
 
+module "balearic_blue_superyacht_services_bucket" {
+  source          = "../modules/s3-bucket"
+  team            = "elite"
+  bucket-use-case = "balearic-blue-superyacht-services"
+  service         = "airflow"
+  versioning      = "Enabled"
+  environment     = var.environment
+}
 
 
