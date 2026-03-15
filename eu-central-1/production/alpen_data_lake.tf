7f0b9c80a1fefc4edab81d0fbf17783dbc366e42
Obinna Ofomah <obinnaofomah@gmail.com>
2026-03-15 07:51:35 +0000

    changed the bucket name

--- eu-central-1/production/alpen_data_lake.tf
+++ eu-central-1/production/alpen_data_lake.tf
@@ -1,7 +1,7 @@
 module "sftp_bucket" {
   source          = "../modules/s3-bucket"
   team            = "elite"
-  bucket-use-case = "sftp-server"
+  bucket-use-case = "sftp"
   service         = "airflow"
   versioning      = "Disabled"
   environment     = var.environment
