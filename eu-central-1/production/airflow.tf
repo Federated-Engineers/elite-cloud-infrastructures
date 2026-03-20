62ec75f301f1ceb648ed0b4e923d9e63bb168130
Jhoie <victoriousjvictor@gmail.com>
2026-03-20 06:18:32 +0100

    Add airflow ssm params policy

--- eu-central-1/production/airflow.tf
+++ eu-central-1/production/airflow.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_policy" "airflow_policy" {
-  name        = "elite-airflow-access-policy"
-  description = "Allow Airflow to access aws resources"
+resource "aws_iam_policy" "airflow_s3_policy" {
+  name        = "elite-airflow-s3-access-policy"
+  description = "Allow Airflow to access S3 buckets"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -21,6 +21,28 @@ resource "aws_iam_policy" "airflow_policy" {
           "${module.balearic_bucket.arn}/*",
           "arn:aws:s3:::gdm-raw-data",
           "arn:aws:s3:::gdm-raw-data/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "airflow_ssm_policy" {
+  name        = "elite-airflow-ssm-access-policy"
+  description = "Allow Airflow to access SSM parameters"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ReadSSMParameters"
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters"
+        ]
+        Resource = [
+          "arn:aws:ssm:eu-central-1:049417293525:parameter/production/google-service-account/credentials",
         ]
       }
     ]
