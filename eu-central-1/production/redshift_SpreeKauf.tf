42b143bc91428812079319c448b8969b4701f175
Basifrank <obasichigoziefrancis@gmail.com>
2026-03-29 14:24:51 -0400

    adding vpc, security groups, and subnet groups

--- eu-central-1/production/redshift_SpreeKauf.tf
+++ eu-central-1/production/redshift_SpreeKauf.tf
@@ -1,9 +1,41 @@
+# creatinng random password for Redshift master user and storing it securely in SSM Parameter Store with KMS encryption
+
 resource "random_password" "password" {
   length           = 16
   special          = true
   min_numeric      = 1
   numeric          = true
   override_special = "!$%&*()-_=+[]{}<>:?"
+}
+
+# Create KMS key for encrypting Redshift master password in SSM Parameter Store
+resource "aws_kms_key" "redshift_predictive" {
+  description             = "KMS key for Redshift SSM parameters"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true # Rotate the key annually — best practice
+}
+
+# Create an alias for the KMS key to make it easier to reference
+resource "aws_kms_alias" "redshift" {
+  name          = "alias/redshift-predictive-ssm"
+  target_key_id = aws_kms_key.redshift_predictive.key_id
+}
+
+# Store the Redshift master password securely in SSM Parameter Store using the KMS key for encryption
+resource "aws_ssm_parameter" "redshift_master_password" {
+  name        = "/prod/redshift-predictive/master-password"
+  description = "This is the master password for SpreeKauf Redshift cluster"
+  type        = "SecureString"
+  value       = random_password.password.result
+  key_id      = aws_kms_key.redshift_predictive.arn
+}
+
+# Store the Redshift master username securely in SSM Parameter Store using the KMS key for encryption
+resource "aws_ssm_parameter" "redshift_master_username" {
+  name        = "/prod/redshift-predictive/master-username"
+  description = "This is the master username for SpreeKauf Redshift cluster"
+  type        = "String"
+  value       = "redshift_admin_user"
 }
 
 
@@ -97,36 +129,86 @@ resource "aws_redshift_parameter_group" "spreekauf_parameter_group" {
 }
 
 
-# Create Redshift Subnet Group
-resource "aws_redshift_subnet_group" "redshift_subnet_group" {
-  name        = "redshift-subnet-group"
-  subnet_ids  = [aws_subnet.secure-production-private-a.id, aws_subnet.secure-production-private-b.id]
-  description = "Redshift subnet group for cluster"
-  tags = {
-    Name = "redshift-subnet-group"
-  }
-  depends_on = [aws_vpc.secure-production]
+data "aws_vpc" "selected" {
+  id = var.vpc_id
 }
 
+data "aws_subnet" "public_a" {
+  id = var.subnet_id_public_a
+}
+
+data "aws_subnet" "public_b" {
+  id = var.subnet_id_public_b
+}
+
+
+# Create Redshift Subnet Group
+resource "aws_redshift_subnet_group" "spreekauf_predictive" {
+  name        = "spreekauf-predictive-subnet-group"
+  description = "Subnet group for SpreeKauf predictive Redshift cluster"
+
+  subnet_ids = [
+    data.aws_subnet.public_a.id,
+    data.aws_subnet.public_b.id
+  ]
+  depends_on = [data.aws_vpc.selected]
+
+  tags = {
+    Name        = "spreekauf-predictive-subnet-group"
+    Environment = "Dev"
+  }
+}
+
+
+# security group for the redshift cluster
+resource "aws_security_group" "redshift_sg_predictive" {
+  name        = "redshift-sg-predictive"
+  description = "Security group for Redshift cluster"
+  vpc_id      = data.aws_vpc.selected.id
+  tags = {
+    Name        = "redshift-sg-predictive"
+    Environment = "Dev"
+  }
+}
+
+# Ingress rules for redshift security group
+resource "aws_security_group_rule" "sg_ingress_https" {
+  security_group_id = aws_security_group.redshift_sg_predictive.id
+  type              = "ingress"
+  description       = "Allow HTTPS traffic from https, http,SQL,RDS,ssh   block"
+  from_port         = [443, 80, 5439, 543, 22]
+  to_port           = [443, 80, 5439, 5432, 22]
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Egress rules for redshift security group
+resource "aws_security_group_rule" "sg_egress_all" {
+  security_group_id = aws_security_group.redshift_sg_predictive.id
+  type              = "egress"
+  description       = "Allow all outbound traffic"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
 
 
 resource "aws_redshift_cluster" "spreekauf_predictive_cluster" {
   cluster_identifier           = "spreekauf-predictive"
-  node_type                    = "dc2.large"
+  node_type                    = "ra3.large"
   cluster_type                 = "multi-node"
-  number_of_nodes              = 1
-  database_name                = "predictive_db"
-  master_username              = "admin"
-  master_password              = random_password.password.result
+  number_of_nodes              = 2
+  database_name                = "spreekauf_database"
+  master_username              = data.aws_ssm_parameter.redshift_master_username.value
+  master_password              = data.aws_ssm_parameter.redshift_master_password.value
   publicly_accessible          = true
   enhanced_vpc_routing         = true
   cluster_parameter_group_name = aws_redshift_parameter_group.spreekauf_parameter_group.name
-  cluster_subnet_group_name    = aws_redshift_subnet_group.redshift_subnet_group.name
-  vpc_security_group_ids       = [aws_security_group.secure-sg.id]
+  cluster_subnet_group_name    = aws_redshift_subnet_group.spreekauf_predictive.name
+  vpc_security_group_ids       = [aws_security_group.redshift_sg_predictive.id]
 
-  skip_final_snapshot = true
-  tags = {
-    Name = "spreekaufRedshiftCluster"
-  }
+  skip_final_snapshot = false
+  tags                = merge(local.common_tags, { Name = "spreekauf-redshift-cluster" })
 }
--- eu-central-1/production/variable.tf
+++ eu-central-1/production/variable.tf
@@ -21,3 +21,24 @@ variable "project" {
   type        = string
   default     = "Federated-Engineers"
 }
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+  default     = "vpc-09a5fdb174ed7c060"
+}
+
+variable "subnet_id_public_a" {
+  description = "The ID of the subnet"
+  type        = string
+  default     = "subnet-0613b8ccd258f4cca"
+}
+
+variable "subnet_id_public_b" {
+  description = "The ID of the subnet"
+  type        = string
+  default     = "subnet-0af2d376a426b58bb"
+}
+
+
+
