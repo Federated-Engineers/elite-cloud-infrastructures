b49dedd76c96a1b65fc39ca7d53a4a3cf90f2a62
Airhunmwunde Joy Iyosayi <iyosayi@Mac.mynet>
2026-06-02 12:50:24 +0100

    create ecr repo

--- eu-central-1/production/ecr.tf
+++ eu-central-1/production/ecr.tf
@@ -138,3 +138,35 @@ resource "aws_ecr_lifecycle_policy" "elite_kings_county_ledger_dbt" {
   policy = data.aws_ecr_lifecycle_policy_document.elite_kings_county_ledger_dbt.json
 }
 
+resource "aws_ecr_repository" "elite_cocosurf_gear_dbt" {
+  name                 = "elite-cocosurf-gear-dbt"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = merge(local.common_tags, { Name = "elite-cocosurf-gear-dbt" })
+}
+
+data "aws_ecr_lifecycle_policy_document" "elite_cocosurf_gear_dbt" {
+  rule {
+    priority    = 1
+    description = "To delete old ecr images but keet the 3 most recent images"
+    action {
+      type = "expire"
+    }
+
+    selection {
+      tag_status   = "any"
+      count_type   = "imageCountMoreThan"
+      count_number = 3
+    }
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "elite_cocosurf_gear_dbt" {
+  repository = aws_ecr_repository.elite_cocosurf_gear_dbt.name
+
+  policy = data.aws_ecr_lifecycle_policy_document.elite_cocosurf_gear_dbt.json
+}
