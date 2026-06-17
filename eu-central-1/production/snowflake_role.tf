b5280ffd9f4fdb2b6ba9205efd39fb21eb31397a
unknown <ojokayode13@gmail.com>
2026-06-17 09:38:12 +0100

    setup snowflake access role and policy

---
+++ eu-central-1/production/snowflake_role.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_role" "elite_snowflake_s3_role" {
+  name        = "elite-lonestar-snowflake-role"
+  description = "Allow Snowflake storage integration to assume role for S3 access"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowSnowflakeAssume"
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          AWS = "049417293525"
+        }
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" = "0000"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "elite_snowflake_s3_policy" {
+  name        = "elite-lonestar-snowflake-policy"
+  description = "Allow Snowflake to read from lonestar-assurance-lake"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Read"
+        Effect = "Allow"
+        Action = [
+          "s3:List*",
+          "s3:Get*"
+        ]
+        Resource = [
+          "arn:aws:s3:::lone-star-assurance-lake",
+          "arn:aws:s3:::lone-star-assurance-lake/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "elite_snowflake_s3_attachment" {
+  role       = aws_iam_role.elite_snowflake_s3_role.name
+  policy_arn = aws_iam_policy.elite_snowflake_s3_policy.arn
+}
