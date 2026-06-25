8e555c49941fd567f716ca0f8d3e963a587a1ad1
Jhoie <victoriousjvictor@gmail.com>
2026-06-25 21:03:04 +0100

    Add lonestar integration details

--- eu-central-1/production/snowflake_role.tf
+++ eu-central-1/production/snowflake_role.tf
@@ -10,11 +10,13 @@ resource "aws_iam_role" "elite_snowflake_s3_role" {
         Effect = "Allow"
         Action = "sts:AssumeRole"
         Principal = {
-          AWS = ["arn:aws:iam::269657857787:user/wv0u1000-s"]
+          AWS = ["arn:aws:iam::650012445037:user/0trs1000-s",
+          "arn:aws:iam::269657857787:user/wv0u1000-s"]
         }
         Condition = {
           StringEquals = {
-            "sts:ExternalId" = ["KY10182_SFCRole=121_VYSuNn1eQ6Bsm66MFrEm6/Ov3A0="]
+            "sts:ExternalId" = ["SQ35311_SFCRole=5_+0fH4prUkj3DW/Sd1ptZfnCLB/E=",
+            "KY10182_SFCRole=121_VYSuNn1eQ6Bsm66MFrEm6/Ov3A0="]
           }
         }
       }
@@ -39,6 +41,8 @@ resource "aws_iam_policy" "elite_snowflake_s3_policy" {
           "s3:ListBucket"
         ]
         Resource = [
+          "arn:aws:s3:::lone-star-assurance-lake",
+          "arn:aws:s3:::lone-star-assurance-lake/*",
           "arn:aws:s3:::kings-county-raw-ingestion",
           "arn:aws:s3:::kings-county-raw-ingestion/*"
         ]
