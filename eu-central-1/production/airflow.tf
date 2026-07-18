00e51ff150caa5e96221b7b679de43581749e98c
Musili-Adebayo <adebayomusili@gmail.com>
2026-07-18 15:28:21 +0100

    updated airflow task definition permission

--- eu-central-1/production/airflow.tf
+++ eu-central-1/production/airflow.tf
@@ -84,7 +84,7 @@ resource "aws_iam_policy" "airflow_ecs_policy" {
         ]
         Resource = [
           aws_ecs_cluster.angel_city_cluster.arn,
-          aws_ecs_task_definition.angel_city_dbt_task.arn,
+          aws_ecs_task_definition.elite_dbt_task.arn,
           aws_ecs_cluster.elite_kings_county_dbt.arn,
           aws_ecs_task_definition.elite_kings_county_dbt_task.arn,
           aws_ecs_cluster.elite_lonestar.arn,
