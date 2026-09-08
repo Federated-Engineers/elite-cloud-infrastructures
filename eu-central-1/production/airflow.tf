3d53977bd5bc7f62767b7303f2c9194dd6a5840a
taofeecoh <adesanutaofeecoh@gmail.com>
2026-09-08 19:19:10 +0100

    add new resource to airflow policy

--- eu-central-1/production/airflow.tf
+++ eu-central-1/production/airflow.tf
@@ -38,7 +38,7 @@ resource "aws_iam_policy" "airflow_policy" {
           module.mare_viva_bucket.arn,
           "${module.mare_viva_bucket.arn}/*",
           module.client_alpenmachanik_sftp_server_storage.arn,
-          "${module.client_alpenmachanik_sftp_server_storage}/*"
+          "${module.client_alpenmachanik_sftp_server_storage.arn}/*"
         ]
       },
 
